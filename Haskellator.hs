810d0920a7ba12439b98e9ef44199151e6d687cd
Curt Bridgers <bridgerspc@appstate.edu>
2021-04-08 19:31:42 -0400

    Bumbed base dependency constraint to support newer versions of GHC

--- Haskellator.hs
+++ Haskellator.hs
@@ -24,8 +24,7 @@ main = do
 
 -- My own state for the program
 data MyState = MyState {
-    evalString :: String,
-    cursor :: NonEmptyCursor String
+    evalString :: String
     }
     deriving (Show, Eq)
 
@@ -36,7 +35,8 @@ type ResourceName = String
 app :: App MyState e ResourceName
 app = App {
     appDraw = drawUI,
-    appChooseCursor = showFirstCursor,
+    -- appChooseCursor = showFirstCursor,
+    appChooseCursor = neverShowCursor,
     appHandleEvent = handleEvent,
     appStartEvent = pure,
     appAttrMap = const $ attrMap mempty [(attrName "selected", fg red)]
@@ -45,23 +45,14 @@ app = App {
 -- Takes a state and returns a list of widgets of
 -- the appropriate resource name
 drawUI :: MyState -> [Widget ResourceName]
-drawUI s = [title, evalText, renderedNumpad]
+drawUI s = [title, evalText]
         where
-              title = hCenter $ str "Haskellator v0.1"
-              evalText = padTop (Pad 2) $hCenter $ border $ str $ evalString s
-              renderedNumpad = let nec = cursor s
-                               in center $ hBox $ concat [
-                                   map (padTop (Pad 1) . str) (reverse $ nonEmptyCursorPrev nec),
-                                   [border $ str $ nonEmptyCursorCurrent nec],
-                                   map (padTop (Pad 1) . str) $ nonEmptyCursorNext nec
-                                  ]
+              title = hCenter $ str $ "Haskellator v" ++ show 0.1
+              evalText = center $ hCenter $ border $ str $ evalString s
 
 -- buildInitState: bui
 buildInitState :: IO MyState
-buildInitState = do
-    let numpad = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
-    pure MyState { evalString = "", cursor = case DNE.nonEmpty numpad of
-                                                      Just np -> makeNonEmptyCursor np }
+buildInitState = pure MyState { evalString = "" }
 
 -- Handling vty events
 handleEvent :: MyState -> BrickEvent n e -> EventM n (Next MyState)
@@ -70,23 +61,9 @@ handleEvent s e =
         VtyEvent vtye ->
             case vtye of
                 EvKey (KChar 'q') [] -> halt s
-                EvKey KRight [] -> continue s'
-                    where s' = case nonEmptyCursorSelectNext (cursor s) of
-                                Nothing -> s
-                                Just newCursor -> MyState { evalString = evalString s, cursor = newCursor }
-                EvKey KLeft [] -> continue s'
-                    where s' = case nonEmptyCursorSelectPrev (cursor s) of
-                                Nothing -> s
-                                Just newCursor -> MyState { evalString = evalString s, cursor = newCursor }
-                EvKey KEnter [] -> continue s'
-                    where s' = MyState { evalString = evalString s ++ [head $ nonEmptyCursorCurrent $ cursor s], cursor = cursor s }
                 EvKey KBS [] -> continue s'
                     where s' = if null $ evalString s then s
-                               else MyState { evalString = init $ evalString s, cursor = cursor s }
-                EvKey (KChar '+') [] -> continue MyState {evalString = evalString s ++ ['+'], cursor = cursor s }
-                EvKey (KChar '-') [] -> continue MyState {evalString = evalString s ++ ['-'], cursor = cursor s }
-                EvKey (KChar '*') [] -> continue MyState {evalString = evalString s ++ ['*'], cursor = cursor s }
-                EvKey (KChar '/') [] -> continue MyState {evalString = evalString s ++ ['/'], cursor = cursor s }
-                EvKey (KChar c) [] -> if isDigit c then continue MyState {evalString = evalString s ++ [c], cursor = cursor s } else continue s
+                               else MyState { evalString = init $ evalString s }
+                EvKey (KChar c) [] -> continue MyState {evalString = evalString s ++ [c] }
                 _ -> continue s
         _ -> continue s
