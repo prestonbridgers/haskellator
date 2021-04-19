d62ac7f4754ad7d6a56f81672bc2adab9edf5a0b
Curt Bridgers <bridgerspc@appstate.edu>
2021-04-19 10:44:07 -0400

    Added lenses and ascii banners for Main, Data Types, and Logic sections of the code

--- Haskellator.hs
+++ Haskellator.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
+import Control.Lens
 
 import Brick
 import Brick.AttrMap
@@ -19,24 +23,21 @@ import Graphics.Vty
 import System.Exit
 import Data.Char
 
-    -- Main
-main :: IO ()
-main = do
-    initState <- buildInitState
-    endState <- defaultMain app initState
-    print endState
-
--- My own state for the program
+--  ____        _          _____                      
+-- |  _ \  __ _| |_ __ _  |_   _|   _ _ __   ___  ___ 
+-- | | | |/ _` | __/ _` |   | || | | | '_ \ / _ \/ __|
+-- | |_| | (_| | || (_| |   | || |_| | |_) |  __/\__ \
+-- |____/ \__,_|\__\__,_|   |_| \__, | .__/ \___||___/
+--                              |___/|_|              
+                      
 data MyState = MyState {
-    evalString :: String,
-    env        :: [(String, Double)]
-    }
-    deriving (Show, Eq)
+    _evalString :: String,
+    _env        :: [(String, Double)]
+    } deriving (Show, Eq)
+makeLenses ''MyState
 
--- Resource name is simply typedef of String
 type ResourceName = String
 
--- Creating the app
 app :: App MyState e ResourceName
 app = App {
     appDraw = drawUI,
@@ -46,23 +47,37 @@ app = App {
     appAttrMap = const $ attrMap mempty [(attrName "selected", fg red)]
     }
 
+--  __  __       _       
+-- |  \/  | __ _(_)_ __  
+-- | |\/| |/ _` | | '_ \ 
+-- | |  | | (_| | | | | |
+-- |_|  |_|\__,_|_|_| |_|
+main :: IO ()
+main = do
+    initState <- pure $ MyState "" []
+    endState <- defaultMain app initState
+    print endState
+
+--  _                _      
+-- | |    ___   __ _(_) ___ 
+-- | |   / _ \ / _` | |/ __|
+-- | |__| (_) | (_| | | (__ 
+-- |_____\___/ \__, |_|\___|
+--             |___/        
+
 -- Takes a state and returns a list of widgets of
 -- the appropriate resource name
 drawUI :: MyState -> [Widget ResourceName]
 drawUI s = [
            (hCenter $ str $ "Haskellator v0.1") <=> (padTop (Pad 1) $ hCenter $ str $ "when giving variables values to use later you must put everything after = in parenthesis.")
             <=> (padTop (Pad 1) $ hCenter $ str $ "example: x = (3+1), y = (4)") <=> (padTop (Pad 1) $ hCenter $ str $ "you can used saved values instead of rewriting the numbers they represent") 
-            <=> (padTop (Pad 2) $ hCenter $ border $ vBox $ map str $ envCon $ env s)
-           ,center $ border $ str $ evalString s 
+            <=> (padTop (Pad 2) $ hCenter $ border $ vBox $ map str $ envCon $ s^.env)
+           ,center $ border $ str $ s^.evalString
            ]
 
 envCon :: [(String, Double)] -> [String] 
 envCon [] = []
 envCon (x:xs) = (fst x ++ " = " ++ (show $ snd x)) : envCon xs
-
--- buildInitState: bui
-buildInitState :: IO MyState
-buildInitState = pure MyState { evalString = "", env = [] }
 
 -- Handling vty events
 handleEvent :: MyState -> BrickEvent n e -> EventM n (Next MyState)
@@ -72,12 +87,12 @@ handleEvent s e =
             case vtye of
             EvKey (KChar 'q') [] -> halt s
             EvKey KBS [] -> continue s'
-                where s' = if null $ evalString s then s
-                           else MyState { evalString = init $ evalString s, env = env s }
-            EvKey (KChar c) [] -> continue MyState {evalString = evalString s ++ [c], env = env s }
+                where s' = if null $ s^.evalString then s
+                           else s & evalString .~ (init $ s^.evalString)
+            EvKey (KChar c) [] -> continue $ s & evalString .~ s^.evalString ++ [c]
             EvKey KEnter [] -> continue s'
-                where s' = case exec (evalString s) (env s) of
-                           (Left newEnv) -> MyState {evalString = "", env = newEnv}
-                           (Right val)     -> MyState {evalString = show val, env = env s}
+                where s' = case exec (s^.evalString) (s^.env) of
+                           (Left newEnv) -> s & (evalString.~"") . (env.~newEnv)
+                           (Right val)   -> s & evalString.~(show val)
             _ -> continue s
         _ -> continue s
