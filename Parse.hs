4638cc2a2aec5df8c9313c9f91d36e9d4c5436bb
Curt Bridgers <bridgerspc@appstate.edu>
2021-05-03 12:19:50 -0400

    Added documentation (comments) to Parse.hs

--- Parse.hs
+++ Parse.hs
@@ -2,6 +2,9 @@ module Parse (exec) where
 
 import Data.Char
 
+
+-- Data types declared here
+
 type Id = String 
 type N = Double 
 
@@ -32,7 +35,6 @@ data Token = CSym Double | VSym String
            | TAss Instr 
            deriving Show 
 
---type of environment for variables to be assigned in
 type Env = [(Id,N)]
 
 --to find values that are associated with given variables
@@ -54,6 +56,10 @@ update :: String -> Double -> Env -> Env
 update s d [] = [(s,d)]
 update s d env = addVar (s,d) (removeVar s env)
 
+-- Evaluates an instruction with respect to a given environment
+-- Returns either an updated Environment in the case of an assignment
+--             or The result of the computation in the form of a double
+--                in the case of an arithmetic expression
 eval :: Instr -> Env -> Either Env Double 
 eval (Assign v e) env = Left (update (v) (exprEval env e) env) 
 eval (AExpr e) env = Right ((exprEval env e))
@@ -97,12 +103,14 @@ isCSym (x:xs) | x `elem` ['0' .. '9'] = isCSym xs
               where isCSym' [] = True
                     isCSym' (y:ys) = if y `elem` ['0' .. '9'] then isCSym' ys else False 
 
+-- Helper function determining valid variable names (IDs)
 isVSym :: String -> Bool 
 isVSym [] = False
 isVSym (x:xs) =  if isLower x then isVSym' xs else False 
                  where isVSym' [] = True
                        isVSym' (x:xs) = if x `elem` ['a'..'z']++['A'..'Z']++['0'..'9']++"-_" then isVSym' xs else False
 
+-- Converts string representation of tokens to the internal representation
 classify :: String -> Token
 classify "(" = LPar
 classify ")" = RPar
@@ -118,35 +126,37 @@ classify x | isVSym x = VSym x
            | isCSym x = CSym (read x :: Double)
            | otherwise = error "classify error: Not a token"
 
--- here we are goin to do all the parsing in this file 
+-- Core parsing function returns singleton list of a token encapsulating
+-- the instruction to be run (can contain either an Assign or AExpr
+-- instruction)
 parse :: [Token] -> [Token] -> [Token]
 parse (CSym n : stack)                    input = parse (TExpr (Const n): stack) input
-parse (VSym v : stack)                   input = parse (TExpr (Var v): stack) input
+parse (VSym v : stack)                    input = parse (TExpr (Var v): stack) input
 parse (RPar: TExpr e : LPar : stack)      input = parse (TExpr e : stack) input -- taking care of parens we just get rid of them 
---- correct precidence
 parse s@(TExpr e2 : BOp op1 : TExpr e1 : stack) (BOp op2 : input) | op1 < op2 = parse (BOp op2 : s) input
-
 parse (TExpr e2 : BOp AddOp : TExpr e1 : stack) input = parse (TExpr (Add e1 e2) : stack) input -- covers add
 parse (TExpr e2 : BOp SubOp : TExpr e1: stack)  input = parse (TExpr(Sub e1 e2) : stack) input -- covers Sub
 parse (TExpr e2 : BOp MulOp : TExpr e1 : stack) input = parse (TExpr (Mul e1 e2) : stack) input 
 parse (TExpr e2 : BOp DivOp : TExpr e1: stack)  input = parse (TExpr(Div e1 e2) : stack) input 
 parse (TExpr e2 : BOp ExpOp : TExpr e1 : stack) input = parse (TExpr (Expo e1 e2) : stack) input 
-
 parse (TExpr e : UOp CosOp : stack)             input = parse (TExpr(Cos e):stack) input -- taking care of cos
 parse (TExpr e : UOp SinOp : stack)             input = parse (TExpr(Sin e):stack) input -- taking care of sin
-
 parse (TExpr e2: BOp OpAss : TExpr (Var v) : stack)     input = parse (TAss (Assign v e2 ): stack ) input -- taking care of an assignmnet 
-
 parse stack (i:input) = parse (i:stack) input
-parse stack [] = stack
+parse stack []        = stack
 
-
+-- This takes the output of the parse function and destructs the token
+-- into the corresponding Instr type
 tokToIn :: [Token] -> Instr
 tokToIn [] = error "Nothing to parse to instruction"
 tokToIn [(TExpr e1)] = AExpr e1 
 tokToIn [(TAss (Assign id e1))] = Assign id e1 
 tokToIn _ = error "Parse Error"
 
+-- Core function that is exposed, takes raw string input and parses/evaluates
+-- it returning:
+-- Either a new environment reflecting an assignment operation
+--     or a double reflecting the result of an arithmetic expression
 exec :: String -> Env -> Either Env Double
 exec s env = eval (run s) env
     where run = tokToIn . parse [] . tokenize
